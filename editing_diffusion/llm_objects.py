b50d77569845ae74c049ecaf72e7b5ff7b7ce7dc
Palandr123 <palandr545@gmail.com>
2024-03-20 18:00:59 +0000

    + spot objects method

--- editing_diffusion/llm_objects.py
+++ editing_diffusion/llm_objects.py
@@ -4,14 +4,16 @@ from transformers.modeling_utils import PreTrainedModel
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 import torch
 
+from editing_diffusion.utils import spot_object_template
+
 
 def get_key_objects(
     tokenizer: PreTrainedTokenizerFast,
     model: PreTrainedModel,
     message: str,
     device: str | torch.device,
-    **model_params
-) -> tuple[dict[str, str], str]:
+    **model_params,
+) -> dict[str, str | list[tuple[str, list[str | None]]]]:
     input_ids = tokenizer.encode(message, return_tensors="pt")
     generated_ids = model.generate(
         input_ids=input_ids.to(device),
@@ -31,11 +33,24 @@ def get_key_objects(
 
     # Extracting additional negative prompt
     bg_prompt = response.split("Background:")[1].split("\n")[0].strip()
-    negative_prompt = response.split("Negation:")[1].strip()
+    negative_prompt = response.split("Negation:")[1].split("\n")[0].strip()
 
     parsed_result = {
         "objects": parsed_objects,
         "bg_prompt": bg_prompt,
         "neg_prompt": negative_prompt,
     }
-    return parsed_result, response
+    return parsed_result
+
+
+def spot_objects(
+    tokenizer: PreTrainedTokenizerFast,
+    model: PreTrainedModel,
+    prompt: str,
+    device: str | torch.device,
+    **model_params,
+) -> dict[str, str | list[tuple[str, list[str | None]]]]:
+    questions = f"User Prompt: {prompt}\nReasoning:\n"
+    message = spot_object_template + questions
+    result = get_key_objects(tokenizer, model, message, device, **model_params)
+    return result
