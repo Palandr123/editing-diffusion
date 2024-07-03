09cce375195341338f7a82d00b5d3034ef3637a2
Palandr123 <palandr545@gmail.com>
2024-07-03 12:09:24 +0000

    + postprocessing

--- editing_diffusion/editing/show_attention.py
+++ editing_diffusion/editing/show_attention.py
@@ -5,7 +5,9 @@ import torch
 from diffusers import StableDiffusionXLPipeline
 
 
-def search_sequence_numpy(arr: np.ndarray[np.int64], seq: np.ndarray[np.int64]) -> np.ndarray[np.int64]:
+def search_sequence_numpy(
+    arr: np.ndarray[np.int64], seq: np.ndarray[np.int64]
+) -> np.ndarray[np.int64]:
     """
     Search for the occurrence of a sequence within a numpy array.
 
@@ -23,8 +25,10 @@ def search_sequence_numpy(arr: np.ndarray[np.int64], seq: np.ndarray[np.int64]) 
     seq_indices = np.arange(seq_length)
 
     # Compare the elements in 'arr' with 'seq' to find matching subsequences.
-    is_match = (arr[np.arange(arr_length - seq_length + 1)[:, None] + seq_indices] == seq).all(1)
-    
+    is_match = (
+        arr[np.arange(arr_length - seq_length + 1)[:, None] + seq_indices] == seq
+    ).all(1)
+
     # If any match is found, return the indices where the sequence is found.
     if is_match.any():
         # Use convolution to find the starting indices of matching sequences.
@@ -32,39 +36,88 @@ def search_sequence_numpy(arr: np.ndarray[np.int64], seq: np.ndarray[np.int64]) 
     return np.array([], dtype=np.int64)  # No match found
 
 
-def get_aux(model: StableDiffusionXLPipeline, apply_tree_map: bool = True, transpose: bool = True) -> dict[str, dict[str, torch.Tensor]]:
-        """
-        Get auxiliary data.
-        This method iterates through named modules in the UNet and collects auxiliary data from them.
-        The `apply_tree_map` and `transpose` parameters control the operations applied to the data.
-        If `transpose` is True, it transposes the data. If `apply_tree_map` is True, it applies tree_map to the data.
+def extract_idxs(words: str | list[str], tokenizer, prompt_text_ids):
+    if not isinstance(words, list):
+        words = [words]
+    idxs = []
+    for word in words:
+        word_ids = tokenizer(word, return_tensors="np")[
+            "input_ids"
+        ]
+        word_ids = word_ids[word_ids < 49406]
+        idxs.append(
+            search_sequence_numpy(prompt_text_ids, word_ids)
+        )
+    return np.concatenate(idxs)
 
-        Args:
-            apply_tree_map: bool - whether to apply tree_map. Default is True.
-            transpose: bool - whether to transpose the data. Default is True.
 
-        Returns:
-            dict[str, dict[str, torch.Tensor]] - the auxiliary data.
-        """
-        auxiliary_data = defaultdict(dict)
-        for name, aux_module in model.unet.named_modules():
-            if hasattr(aux_module, '_aux'):
-                module_auxiliary = aux_module._aux
-                if transpose:
-                    for key, value in module_auxiliary.items():
-                        if apply_tree_map:
-                            value = torch.utils._pytree.tree_map(
-                                lambda vv: vv.chunk(2)[1] if vv is not None else None,
-                                value,
-                            )
-                        auxiliary_data[key][name] = value
-                else:
-                    auxiliary_data[name] = module_auxiliary
+
+def get_aux(
+    model: StableDiffusionXLPipeline,
+    apply_tree_map: bool = True,
+    transpose: bool = True,
+) -> dict:
+    """
+    Get auxiliary data.
+    This method iterates through named modules in the UNet and collects auxiliary data from them.
+    The `apply_tree_map` and `transpose` parameters control the operations applied to the data.
+    If `transpose` is True, it transposes the data. If `apply_tree_map` is True, it applies tree_map to the data.
+
+    Args:
+        apply_tree_map: bool - whether to apply tree_map. Default is True.
+        transpose: bool - whether to transpose the data. Default is True.
+
+    Returns:
+        dict - the auxiliary data.
+    """
+    auxiliary_data = defaultdict(dict)
+    for name, aux_module in model.unet.named_modules():
+        if hasattr(aux_module, "_aux"):
+            module_auxiliary = aux_module._aux
+            if transpose:
+                for key, value in module_auxiliary.items():
                     if apply_tree_map:
-                        auxiliary_data[name] = {
-                            k: torch.utils._pytree.tree_map(
-                                lambda vv: vv.chunk(2)[1] if vv is not None else None, v
-                            )
-                            for k, v in auxiliary_data[name].items()
-                        }
-        return auxiliary_data
+                        value = torch.utils._pytree.tree_map(
+                            lambda vv: vv.chunk(2)[1] if vv is not None else None,
+                            value,
+                        )
+                    auxiliary_data[key][name] = value
+            else:
+                auxiliary_data[name] = module_auxiliary
+                if apply_tree_map:
+                    auxiliary_data[name] = {
+                        k: torch.utils._pytree.tree_map(
+                            lambda vv: vv.chunk(2)[1] if vv is not None else None, v
+                        )
+                        for k, v in auxiliary_data[name].items()
+                    }
+    return auxiliary_data
+
+
+def post_process(aux: dict, n_img: int = 1, aux_idx: int = 0) -> dict:
+    return {
+        k: torch.utils._pytree.tree_map(
+            lambda x: x[aux_idx : aux_idx + 1].repeat_interleave(n_img, 0).cpu(), v
+        )
+        for k, v in aux.items()
+    }
+
+
+def show_attention(
+    model: StableDiffusionXLPipeline,
+    aux: dict,
+    prompt: str,
+    words: str,
+    layers: list[str],
+    timestep: int,
+) -> torch.Tensor:
+    prompt_text_ids = model.tokenizer(prompt, return_tensors="np")["input_ids"][0]
+    word_ids = model.tokenizer(words, return_tensors="np")["input_ids"]
+    word_ids = word_ids[word_ids < 49406]
+    idxs = search_sequence_numpy(prompt_text_ids, word_ids)
+    print(idxs)
+    return {
+        k: v[timestep][0][..., idxs].mean(-1)[None, ..., None][0][..., 0]
+        for k, v in aux["attn"].items()
+        if k in layers
+    }
