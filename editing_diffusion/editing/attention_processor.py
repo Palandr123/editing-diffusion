8a8cb675c370a2434213d892a3eff4f1a1e75641
Palandr123 <palandr545@gmail.com>
2024-07-03 12:08:30 +0000

    + Self-Attention Processor

--- editing_diffusion/editing/attention_processor.py
+++ editing_diffusion/editing/attention_processor.py
@@ -1,5 +1,7 @@
 import math
+import hashlib
 
+import torch
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 from diffusers.models.attention_processor import Attention
@@ -9,6 +11,14 @@ from diffusers.models.unet_2d_blocks import (
     UNetMidBlock2DCrossAttn,
 )
 from diffusers import StableDiffusionXLPipeline
+
+
+def hash_tensor(tensor: torch.Tensor):
+    tensor_bytes = (
+        tensor.detach().cpu().numpy().tobytes()
+    )  # Detach and convert to bytes
+    h = hashlib.sha256(tensor_bytes)
+    return h.hexdigest()
 
 
 class CustomAttentionProcessor:
@@ -28,7 +38,7 @@ class CustomAttentionProcessor:
     def __call__(
         self,
         attn: Attention,
-        hidden_states,
+        hidden_states: torch.Tensor,
         encoder_hidden_states=None,
         attention_mask=None,
         temb=None,
@@ -154,3 +164,118 @@ def register_attention(
                 full_name = name + "." + attn_name
                 if isinstance(attn, Attention) and full_name in target_attention_list:
                     attn.processor = CustomAttentionProcessor()
+
+
+class SelfAttnProccessor:
+    r"""
+    Processor for implementing scaled dot-product attention (enabled by default if you're using PyTorch 2.0).
+    """
+
+    def __init__(self, save_aux: bool = True, num_iterations = 300):
+        if not hasattr(F, "scaled_dot_product_attention"):
+            raise ImportError(
+                "AttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0."
+            )
+        self.save_aux = save_aux
+        self.buffer = []
+        self.idx = 0
+        self.num_iterations = num_iterations
+
+    def __call__(
+        self,
+        attn: Attention,
+        hidden_states: torch.Tensor,
+        encoder_hidden_states: torch.Tensor | None = None,
+        attention_mask: torch.Tensor | None = None,
+        temb: torch.Tensor | None = None,
+        *args,
+        **kwargs,
+    ) -> torch.Tensor:
+        residual = hidden_states
+        if attn.spatial_norm is not None:
+            hidden_states = attn.spatial_norm(hidden_states, temb)
+
+        input_ndim = hidden_states.ndim
+
+        if input_ndim == 4:
+            batch_size, channel, height, width = hidden_states.shape
+            hidden_states = hidden_states.view(
+                batch_size, channel, height * width
+            ).transpose(1, 2)
+
+        batch_size, sequence_length, _ = (
+            hidden_states.shape
+            if encoder_hidden_states is None
+            else encoder_hidden_states.shape
+        )
+
+        if attention_mask is not None:
+            attention_mask = attn.prepare_attention_mask(
+                attention_mask, sequence_length, batch_size
+            )
+            # scaled_dot_product_attention expects attention_mask shape to be
+            # (batch, heads, source_length, target_length)
+            attention_mask = attention_mask.view(
+                batch_size, attn.heads, -1, attention_mask.shape[-1]
+            )
+
+        if attn.group_norm is not None:
+            hidden_states = attn.group_norm(hidden_states.transpose(1, 2)).transpose(
+                1, 2
+            )
+
+        query = attn.to_q(hidden_states)
+
+        if encoder_hidden_states is None:
+            encoder_hidden_states = hidden_states
+        elif attn.norm_cross:
+            encoder_hidden_states = attn.norm_encoder_hidden_states(
+                encoder_hidden_states
+            )
+
+        key = attn.to_k(encoder_hidden_states)
+        value = attn.to_v(encoder_hidden_states)
+
+        if self.save_aux:
+            self.buffer.append({"key": key.detach().cpu(), "value": value.detach().cpu()})
+        else:
+            key = self.buffer[self.idx]["key"].to(key.device)
+            value = self.buffer[self.idx]["value"].to(value.device)
+            self.idx += 1
+            self.idx %= self.num_iterations
+
+        inner_dim = key.shape[-1]
+        head_dim = inner_dim // attn.heads
+
+        query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+
+        key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+        value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+
+        # the output of sdp = (batch, num_heads, seq_len, head_dim)
+        # TODO: add support for attn.scale when we move to Torch 2.1
+        hidden_states = F.scaled_dot_product_attention(
+            query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
+        )
+
+        hidden_states = hidden_states.transpose(1, 2).reshape(
+            batch_size, -1, attn.heads * head_dim
+        )
+        hidden_states = hidden_states.to(query.dtype)
+
+        # linear proj
+        hidden_states = attn.to_out[0](hidden_states)
+        # dropout
+        hidden_states = attn.to_out[1](hidden_states)
+
+        if input_ndim == 4:
+            hidden_states = hidden_states.transpose(-1, -2).reshape(
+                batch_size, channel, height, width
+            )
+
+        if attn.residual_connection:
+            hidden_states = hidden_states + residual
+
+        hidden_states = hidden_states / attn.rescale_output_factor
+
+        return hidden_states
